0c248e0f55d5cbb2f0a4a2aa91a3907fe0d51a79
fdreer <francodreer@gmail.com>
2023-01-19 20:37:14 -0300

    Componentes ta te ti finalizados

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,15 +1,66 @@
 import {useState} from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import confetti from 'canvas-confetti';
+import Square from './components/Square';
+import {TURNS} from './constants';
+import {checkEndGame, checkWinner} from './logic/board';
+import WinnerModal from './components/WinnerModal';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.x);
+
+  // null es que no hay ganador, false un empate
+  const [winner, setWinner] = useState(null);
+
+  const updateBoard = (index) => {
+    // no actualizamos el estado
+    // si ya tiene algo en esa posicion
+    if (board[index] || winner) return;
+
+    // actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    // cambiar el turno
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
+    setTurn(newTurn);
+
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); //empate
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.x);
+    setWinner(null);
+  };
 
   return (
-    <div className="App">
-      <h1>Ta te ti</h1>
-    </div>
+    <main className="board">
+      <h1>TA TE TI</h1>
+      <button onClick={resetGame}>Reset Game</button>
+      <section className="game">
+        {board.map((square, index) => (
+          <Square key={index} index={index} updateBoard={updateBoard}>
+            {square}
+          </Square>
+        ))}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
+        <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
   );
 }
-
-export default App;
---
+++ projects/02-ta-te-ti/src/components/WinnerModal.jsx
@@ -0,0 +1,16 @@
+export default function WinnerModal({winner, resetGame}) {
+  if (winner === null) return null;
+  const winnerText = winner === false ? 'Empate' : 'Gano';
+
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
+        <footer>
+          <button onClick={resetGame}>Volver a jugar</button>
+        </footer>
+      </div>
+    </section>
+  );
+}
